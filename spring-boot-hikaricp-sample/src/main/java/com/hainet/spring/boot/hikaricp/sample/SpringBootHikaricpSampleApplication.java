97fa205a01ed341f1dd105b80ede177d2f2fbf01
hainet <20400616+hainet@users.noreply.github.com>
2017-12-27 18:27:02 +0900

    DBCP2のサンプル追加に伴いディレクトリ構成を変更

--- spring-boot-hikaricp-sample/src/main/java/com/hainet/spring/boot/hikaricp/sample/SpringBootHikaricpSampleApplication.java
+++ spring-boot-hikaricp-sample/src/main/java/com/hainet/spring/boot/hikaricp/sample/SpringBootHikaricpSampleApplication.java
@@ -2,24 +2,33 @@ package com.hainet.spring.boot.hikaricp.sample;
 
 import com.hainet.spring.boot.hikaricp.sample.domain.dao.SessionDao;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
 @EnableScheduling
 @RequiredArgsConstructor
-public class SpringBootHikaricpSampleApplication {
+public class SpringBootHikaricpSampleApplication implements CommandLineRunner {
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         SpringApplication.run(SpringBootHikaricpSampleApplication.class, args);
     }
 
+    private final DataSource dataSource;
     private final SessionDao dao;
 
-    @Scheduled(fixedDelay = 5_000)
-    public void test() {
+    @Override
+    public void run(final String... args) {
+        System.out.println(dataSource);
+    }
+
+    @Scheduled(fixedDelay = 3_000)
+    public void hikaricp() {
         System.out.println(dao.findAll());
     }
 }
