436a78ca9ce912d0ded6f2e38e219c48c5c8d655
hainet <20400616+hainet@users.noreply.github.com>
2018-01-11 13:39:46 +0900

    コンテナ生成のテストを追加

---
+++ spring-boot-dbcp2-sample/src/test/java/com/hainet/springbootdbcp2sample/SpringBootDbcp2SampleApplicationTest.java
@@ -0,0 +1,15 @@
+package com.hainet.springbootdbcp2sample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootDbcp2SampleApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+}
